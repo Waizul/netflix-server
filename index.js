5f674caa231c9be847fef9528e160c9d4bc71cbc
Waizul Haque <waizul@gmail.com>
2022-02-08 01:20:38 +0600

    movie and list

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ require( 'dotenv' ).config();
 
 const authRoute= require('./routes/auth')
 const userRoute= require('./routes/users')
+const movieRoute= require('./routes/movies')
+const listRoute= require('./routes/lists')
 
 mongoose
 	.connect(process.env.DB_URL)
@@ -17,6 +19,8 @@ app.use( express.json() )
 
 app.use( '/auth', authRoute )
 app.use( '/users', userRoute )
+app.use( '/movies', movieRoute )
+app.use( '/lists', listRoute )
 
 app.listen(8800, () => {
 	console.log('server is running at ', 8800);
